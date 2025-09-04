ce158ed435968c9e27a8ab953a23380e745efb1f
SamyTheSpheal <lsjimenez363@gmail.com>
2025-09-03 23:55:27 -0700

    jumping :)

--- Assets/PhysicsBase.cs
+++ Assets/PhysicsBase.cs
@@ -6,7 +6,9 @@ public class PhysicsBase : MonoBehaviour
 {
 
     public Vector2 velocity;
-    public float gravityFactor; 
+    public float gravityFactor;
+    public float desiredx;
+    public bool grounded;
 
     // Start is called before the first frame update
     void Start() // _its like _ready()
@@ -14,25 +16,41 @@ public class PhysicsBase : MonoBehaviour
         
     }
 
-    public void Movement(Vector2 move) { 
+    public void Movement(Vector2 move, bool horizontal) { 
         if (move.magnitude < .00001f) return;
-
+        grounded = false;
         RaycastHit2D[] results = new RaycastHit2D[16];
         int cnt = GetComponent<Rigidbody2D>().Cast(move, results, move.magnitude + .001f);
 
-        if (cnt > 0) return;
+        for (int i = 0; i < cnt; i++)
+        {
+            if (Mathf.Abs(results[i].normal.x) > .3f && horizontal)
+            {
+                return;
+            }
+            if (Mathf.Abs(results[i].normal.y) > .3f && !horizontal)
+            {
+                if (results[i].normal.y > .3f) grounded = true;
+                return;
+            }    
+        }
   
 
         transform.position += (Vector3)move;
     }
 
     // Update is called once per frame
-    void Update() // its like _process()
+    void FixedUpdate() // its like _process()
     {
         Vector2 acceleration = 9.81f * Vector2.down * gravityFactor;
-        velocity += acceleration * Time.deltaTime;
+        velocity += acceleration * Time.fixedDeltaTime;
+        velocity.x = desiredx;
+        Vector2 move = velocity * Time.fixedDeltaTime;
 
-        Movement(velocity * Time.deltaTime);
+        Movement(new Vector2(move.x,0), true);
+        Movement(new Vector2(0, move.y), false);
+
+        if (Input.GetButton("Jump") && grounded) velocity.y = 6.5f;
 
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : PhysicsBase
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -16,9 +15,11 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxis("Horizontal") > 0) desiredx = 3;
         else if (Input.GetAxis("Horizontal") < 0) desiredx = -3;
         else desiredx = 0;
-        if (Input.GetButton("Jump") && grounded)
+        /*if (Input.GetButton("Jump") && grounded)
         {
             velocity.y = 6.5f;
-        }
+        }*/
     }
+
+
 }
